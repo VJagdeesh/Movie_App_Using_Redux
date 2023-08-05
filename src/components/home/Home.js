b58c0ca687031ad59e2b7979a262e0c7e7b1b7a8
unknown <jagdeeshbhavani22@gmail.com>
2023-08-05 10:47:39 +0530

    Second commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import MovieListing from "../movieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
-
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 const Home = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
     const movieText = "Titanic";
     const fetchMovies = async () => {
@@ -12,7 +14,8 @@ const Home = () => {
         .catch((err) => {
           console.log("Error");
         });
-      console.log("Response ", response);
+      //   console.log("Response ", response.data);
+      dispatch(addMovies(response.data));
     };
     // fetchMovies();
   }, []);
